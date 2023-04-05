2487a2a85e72e89bbfa58903d61d3bcb6b7e2d54
Leannon <118322766+LeannonS@users.noreply.github.com>
2023-04-05 17:38:56 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,22 @@ using namespace std;
 int main() 
 {
   Labirinto l(10, 0, 0, 0, 0);
-  int flag;
 
   l.criandoArquivoAuxiliar();
+  l.criandoArquivoVerificador();
   l.criandoLabirinto();
   l.pegandoValoresLabirinto();
-  l.deletandoLabirinto();
 
-  cout << "Casas percorridas ao todo: " << l.getPassos() << endl;
+  cout << endl << "Casas percorridas ao todo: " << l.getPassos() << endl;
   cout << "Soma de itens coletados pelo caminho: " << l.getItensPegos() << endl;
+  cout << "Número de casas não exploradas no labirinto: " << l.verificarCasasInexploradas() << endl;
   cout << "Perigos enfrentados durante o caminho: " << l.getPerigos() << endl;
 
+  l.deletandoLabirinto();
   remove("arquivoAuxiliar.txt");
   remove("segundoArquivoAuxiliar.txt");
+  remove("arquivoVerificador.txt");
+  remove("segundoArquivoVerificador.txt");
   
   return 0;
 }
